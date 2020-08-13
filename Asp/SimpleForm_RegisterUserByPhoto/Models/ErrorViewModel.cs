2fab82dde90a776a8722a7c12c32497d059cf2d3
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-13 15:21:27 +0430

    Create project SimpleForm_RegisterUserByPhoto.

---
+++ Asp/SimpleForm_RegisterUserByPhoto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleForm_RegisterUserByPhoto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
