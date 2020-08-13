33305b9d9971ee5c5c6596fdc231797e75d5e7ef
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-13 16:14:00 +0430

    Add models.

---
+++ Asp/SimpleForm_RegisterUserByPhoto/Models/Gender.cs
@@ -0,0 +1,6 @@
+namespace SimpleForm_RegisterUserByPhoto.Models {
+    public class Gender {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Asp/SimpleForm_RegisterUserByPhoto/Models/Person.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleForm_RegisterUserByPhoto.Models {
+    public class Person {
+        public int Id { get; set; }
+
+        [StringLength (35)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength (35)]
+        public string Family { get; set; }
+
+        [Range (5, 120)]
+        public int Age { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+        public string Description { get; set; }
+        public bool Agreement { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+    }
+}
