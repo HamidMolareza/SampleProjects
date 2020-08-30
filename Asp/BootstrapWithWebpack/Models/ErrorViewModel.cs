e90104d0f4bac68426a8b6a018a88c3dcb8975dd
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-30 19:16:08 +0430

    First commit.

---
+++ Asp/BootstrapWithWebpack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootstrapWithWebpack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
